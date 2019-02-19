dae271929127e6cb36e83281d5ac66ddb9b43ace
namachan10777 <namachan10777@gmail.com>
2019-02-20 03:17:38 +0900

    impl trie!

--- bot/src/morph.rs
+++ bot/src/morph.rs
@@ -32,11 +32,12 @@ struct Trie {
     infos: Vec<WordInfo>
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug, PartialEq)]
 enum wclass {
+    Dummy,
 }
 
-#[derive(Clone)]
+#[derive(Clone, Debug, PartialEq)]
 struct WordInfo {
     lid: u16,
     rid: u16,
@@ -49,18 +50,18 @@ const ROW_LEN: usize = 256;
 
 impl Trie {
     fn new() -> Trie {
-        let mut fst = [Node::default(); 257].to_vec();
-        fst[0] = Node { base: 1, check: 0, ptr: 0 };
+        let mut arr = vec![Node::default(); ROW_LEN+1].to_vec();
+        arr[0] = Node { base: 1, check: 0, ptr: 0 };
         Trie {
-            arr: fst,
+            arr: arr,
             infos: Vec::new(),
         }
     }
 
     // 経路を辿り、辿りきれば終点のindexを、辿りきれなければ(終点のindex, 辿れた数)を返す
-    fn pursue(&self, parent: usize, octets: &Vec<u8>) -> Result<usize, (usize, usize)> {
-        let mut check: usize = self.arr[parent].check;
-        let mut child_id: usize = parent;
+    fn pursue(&self, octets: &Vec<u8>) -> Result<usize, (usize, usize)> {
+        let mut check: usize = self.arr[0].check;
+        let mut child_id: usize = 0;
         for i in 0..octets.len() {
             let new_child_id = self.arr[child_id].base + octets[i] as usize;
             if new_child_id >= self.arr.len() || self.arr[new_child_id].check != child_id {
@@ -107,7 +108,9 @@ impl Trie {
     fn place(&mut self, nodes: &Vec<Node>) -> usize {
         let p = self.find_placeable_pos(&nodes);
         for i in 0..nodes.len() {
-            self.arr[i+p] = nodes[i];
+            if nodes[i].check != NOWHERE {
+                self.arr[i+p] = nodes[i];
+            }
         }
         p
     }
@@ -131,14 +134,64 @@ impl Trie {
         buf
     }
     
-    fn push_out(&mut self, base: usize, octet: usize) {
-        let occupy_parent = self.arr[base + octet].check;
+    fn push_out(&mut self, idx: usize) {
+        let base = self.arr[idx].base;
+        let occupy_parent = self.arr[idx].check;
         let occupy_row = self.extract_row(base, occupy_parent);
         self.erase(base, occupy_parent);
-        self.arr[base + octet].check = 0;
+        // 再配置防止に
+        self.arr[idx].check = 0;
         let occupy_base = self.place(&occupy_row);
         self.arr[occupy_parent].base = occupy_base;
-        self.arr[base + octet].check = NOWHERE;
+        self.arr[idx].check = NOWHERE;
+    }
+
+    fn add(&mut self, octets: &Vec<u8>, info: WordInfo) {
+        if let Err((common, pursued)) = self.pursue(octets) {
+            let mut current = self.arr[common].base + octets[pursued] as usize;
+            println!("{:?} {:?}", common, pursued);
+            // 終端ノード
+            if self.arr[common].base == 0 {
+                println!("終端");
+                let mut row = [Node::default(); ROW_LEN].to_vec();
+                row[octets[pursued] as usize].check = common;
+                let base = self.place(&row);
+                self.arr[common].base = base;
+
+                current = base + octets[pursued] as usize;
+            }
+            // 非終端ノードかつ衝突あり
+            else if self.arr[current].check != NOWHERE {
+                println!("非終端 衝突");
+                self.push_out(current);
+            }
+            else {
+                println!("非終端");
+            }
+
+            let mut parent = common;
+
+            for i in pursued..octets.len() {
+                println!("更新 {}", parent);
+                let mut row = [Node::default(); ROW_LEN].to_vec();
+                row[octets[i] as usize].check = parent;
+                let base = self.place(&row);
+                self.arr[parent].base = base;
+                parent = base + octets[i] as usize;
+            }
+
+            self.infos.push(info);
+            self.arr[parent].ptr = self.infos.len() - 1;
+        }
+    }
+
+    fn find(&mut self, octets: &Vec<u8>) -> Option<WordInfo> {
+        if let Ok(idx) = self.pursue(octets) {
+            Some(self.infos[self.arr[idx].ptr].clone())
+        }
+        else {
+            None
+        }
     }
 }
 #[cfg(test)]
@@ -182,13 +235,26 @@ mod trie_test {
             // 7
             Node { base: 8, check: 5, ptr: 0 }
         ].to_vec();
-        assert_eq!(trie.pursue(0, &vec![0, 1]), Err((0, 0)));
-        assert_eq!(trie.pursue(0, &vec![1, 1]), Ok(5));
-        assert_eq!(trie.pursue(0, &vec![1, 1, 0]), Err((5, 2)));
-        assert_eq!(trie.pursue(0, &vec![1, 2]), Err((2, 1)));
-        assert_eq!(trie.pursue(0, &vec![2, 0, 1]), Err((6, 2)));
-        assert_eq!(trie.pursue(0, &vec![2, 0]), Ok(6));
-        assert_eq!(trie.pursue(0, &vec![1, 1, 1]), Ok(7));
+        assert_eq!(trie.pursue(&vec![0, 1]), Err((0, 0)));
+        assert_eq!(trie.pursue(&vec![1, 1]), Ok(5));
+        assert_eq!(trie.pursue(&vec![1, 1, 0]), Err((5, 2)));
+        assert_eq!(trie.pursue(&vec![1, 2]), Err((2, 1)));
+        assert_eq!(trie.pursue(&vec![2, 0, 1]), Err((6, 2)));
+        assert_eq!(trie.pursue(&vec![2, 0]), Ok(6));
+        assert_eq!(trie.pursue(&vec![1, 1, 1]), Ok(7));
+    }
+
+    #[test]
+    fn test_add() {
+        let mut trie = Trie::new();
+        let w1 = WordInfo { lid: 0, rid: 0, cost: 1, wclass: wclass::Dummy };
+        let w2 = WordInfo { lid: 0, rid: 0, cost: 2, wclass: wclass::Dummy };
+        let w3 = WordInfo { lid: 0, rid: 0, cost: 3, wclass: wclass::Dummy };
+        trie.add(&vec![0], w1.clone());
+        trie.add(&vec![0, 1], w2.clone());
+        trie.add(&vec![1, 2, 3], w3.clone());
+        assert_eq!(trie.find(&vec![0]), Some(w1));
+        assert_eq!(trie.find(&vec![1, 2, 3]), Some(w3));
     }
 }
 
