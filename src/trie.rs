1eed4ac6bffb669c34ba23edf19f49f2b9ddb982
namachan10777 <namachan10777@gmail.com>
2019-08-04 02:09:08 +0900

    impl Node decoder

--- src/trie.rs
+++ src/trie.rs
@@ -1,13 +1,44 @@
 // copyright (c) 2019 Nakano Masaki <namachan10777@gmail>
 use std::usize;
 
+#[derive(Clone)]
 struct Node {
-    // rowのオフセット
     base: usize,
-    // 親ノードのindex
     check: usize,
-    // 格納している要素のid
-    ptr: Option<usize>,
+    id: usize,
+}
+
+enum DecodedNode {
+    Root(usize),
+    Sec(usize, usize, Option<usize>),
+    Term(usize, usize),
+}
+
+impl Node {
+    fn decode(&self) -> DecodedNode {
+        // 00 -> Root
+        // 01 -> Term
+        // 10 -> Sec
+        // 11 -> Sec (with data)
+        let stand1 = 0x8000000000000000;
+        let stand2 = 0x4000000000000000;
+        return if self.id & stand1 == stand1 {
+            if self.id & stand2 == stand2 {
+                DecodedNode::Term(self.check, self.id & 0x2fffffffffffffff)
+            }
+            else {
+                DecodedNode::Root(self.base)
+            }
+        }
+        else {
+            if self.id & stand2 == stand2 {
+                DecodedNode::Sec(self.base, self.check, Some(self.id & 0x2fffffffffffffff))
+            }
+            else {
+                DecodedNode::Sec(self.base, self.check, None)
+            }
+        }
+    }
 }
 
 struct Trie<T> {
