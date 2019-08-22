3caae5c2271ed9723a85262636e191217bcd5516
namachan10777 <namachan10777@gmail.com>
2019-08-23 03:41:19 +0900

    measure callgrind on CircleCI

--- profile.sh
+++ profile.sh
@@ -2,7 +2,4 @@
 cd $(dirname $0)
 
 cargo build --release
-valgrind --tool=callgrind --dump-instr=yes --collect-jumps=yes --simulate-cache=yes ./target/release/bot
-for f in callgrind.out.*;do
-	mv $f ./callgrind/callgrind.out
-done
+valgrind --tool=callgrind --dump-instr=yes --collect-jumps=yes --simulate-cache=yes ./target/release/namaco
