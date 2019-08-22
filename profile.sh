49f4eae5e845459f02bac49ab8fb0ab163a9ccb6
namachan10777 <namachan10777@gmail.com>
2019-08-23 03:47:57 +0900

    fix measure callgrind script

--- profile.sh
+++ profile.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
 cd $(dirname $0)
 
+rm callgrind.out.*
 cargo build --release
 valgrind --tool=callgrind --dump-instr=yes --collect-jumps=yes --simulate-cache=yes ./target/release/namaco
+mv callgrind.out.* callgrind.out
