c6bf5182df2d76a14bb5b5be15e76de2332716e1
namachan10777 <namachan10777@gmail.com>
2019-08-23 03:07:39 +0900

    Can't measure coverage on CircleCI

--- coverage.sh
+++ coverage.sh
@@ -5,15 +5,4 @@ HERE=$(cd $(dirname $0); pwd)
 cd $HERE
 cargo clean
 RUSTFLAGS="-C link-dead-code" cargo test --no-run
-REPORT=$(find target/debug -maxdepth 1 -name 'namaco-*' -a ! -name '*.d')
-
-for file in $REPORT; do
-	echo $file
-    mkdir -p "target/cov/$(basename $file)"
-    kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"
-done
-
-wget -O - -q "https://codecov.io/bash" > .codecov
-chmod +x .codecov
-./.codecov -t $CODECOV_TOKEN
-echo "Uploaded code coverage"
+cargo kcov --no-clean-rebuild --verbose --all --no-fail-fast
