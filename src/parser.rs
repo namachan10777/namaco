a5ba93018e683070adabce50ecb714773c912d2c
namachan10777 <namachan10777@gmail.com>
2019-09-20 21:33:43 +0900

    test parser

--- src/parser.rs
+++ src/parser.rs
@@ -4,12 +4,10 @@ pub struct DictCfg {
     gencost: usize,
 }
 
-#[allow(dead_code)]
+#[derive(Debug, PartialEq)]
 pub struct Word<T> {
     info: T,
-
     word: String,
-
     gencost: i64,
     matrix_id: usize,
 }
@@ -32,15 +30,14 @@ pub fn parse_naist_jdic_by_line<F, T>(cfg: &DictCfg, classifier: F, line: &str) 
     }
 }
 
-use std::fs;
 use std::io;
-use std::io::{BufRead};
+use std::io::{BufRead, Read};
 
 #[allow(dead_code)]
-pub fn parse_csv_dict<F, T>(file: &fs::File, cfg: &DictCfg, classifier: F) -> Result<Vec<Word<T>>, io::Error>
+pub fn parse_csv_dict<R: Read, F, T>(readable: R, cfg: &DictCfg, classifier: F) -> Result<Vec<Word<T>>, io::Error>
     where F: Fn(&[&str]) -> T
 {
-    let mut reader = io::BufReader::new(file);
+    let mut reader = io::BufReader::new(readable);
     let mut buf = String::new();
     let mut result = Vec::new();
     while reader.read_line(&mut buf)? > 0 {
@@ -52,7 +49,36 @@ pub fn parse_csv_dict<F, T>(file: &fs::File, cfg: &DictCfg, classifier: F) -> Re
 
 #[cfg(test)]
 mod test_parser {
+    use super::*;
     #[test]
     fn test_parser () {
+        let csv = "蟹,0,100,カニ\n土,1,200,ツチ\n味,2,300,アジ";
+        let cfg = DictCfg {
+            word: 0,
+            matrix_id: 1,
+            gencost: 2,
+        };
+        let result: Result<Vec<Word<String>>, _> =
+            parse_csv_dict(csv.as_bytes(), &cfg, |arr| arr[3].trim().to_string());
+        assert_eq!( result.unwrap(), vec![
+            Word {
+                matrix_id: 0,
+                gencost: 100,
+                word: "蟹".to_string(),
+                info: "カニ".to_string(),
+            },
+            Word {
+                matrix_id: 1,
+                gencost: 200,
+                word: "土".to_string(),
+                info: "ツチ".to_string(),
+            },
+            Word {
+                matrix_id: 2,
+                gencost: 300,
+                word: "味".to_string(),
+                info: "アジ".to_string(),
+            },
+        ]);
     }
 }
