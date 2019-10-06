bf15b10b65646a23319c1470dbb08a7340c9736f
namachan10777 <namachan10777@gmail.com>
2019-10-07 00:08:25 +0900

    fix profile.sh

--- profile.sh
+++ profile.sh
@@ -4,7 +4,7 @@ cd $(dirname $0)
 rm callgrind.out.*
 cargo build
 
-cat ./naist-jdic/naist-jdic.csv | shuf -n 100 | nkf > ./short.csv
+cat ./naist-jdic/naist-jdic.csv | shuf -n 30 | nkf > ./short.csv
 
-valgrind --tool=callgrind --dump-instr=yes --collect-jumps=yes --simulate-cache=yes ./target/debug/namaco
+valgrind --tool=callgrind --dump-instr=yes --collect-jumps=yes --simulate-cache=yes ./target/debug/namaco ./short.csv
 mv callgrind.out.* callgrind.out
