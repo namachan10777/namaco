4aefd69fba6a362a5d60147ac980e5e0791de178
namachan10777 <namachan10777@gmail.com>
2019-10-06 18:00:37 +0900

    recieve dictionary from command line

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 // copyright (c) 2019 Nakano Masaki <namachan10777@gmail.com>
 
-extern crate namaco;
+use clap;
 use namaco::parser;
 use std::fs;
 
 fn main() {
-    let file = fs::File::open("./short.csv").unwrap();
+    let matches = clap::App::new("namaco")
+        .version("0.0.1")
+        .author("Nakano Masaki<namachan10777@gmail.com>")
+        .about("morphological analyzer")
+        .arg(clap::Arg::with_name("DICT")
+            .help("set naist-jdic.csv")
+            .required(true)
+            .index(1))
+        .get_matches();
+    let file = fs::File::open(matches.value_of("DICT").unwrap()).unwrap();
     let cfg = parser::DictCfg {
         word: 0,
-        matrix_id: 1,
-        gencost: 2,
+        matrix_id: 2,
+        gencost: 4,
     };
     let _trie = parser::build_trie(&file, &cfg, |arr| arr[3].trim().to_string()).unwrap();
 }
