9d15442ad3d165f8bc76533fb331f88f034de283
Nakano Masaki <namachan10777@gmail.com>
2019-10-11 16:53:26 +0900

    impl cli frontend

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 // copyright (c) 2019 Nakano Masaki <namachan10777@gmail.com>
 
 use clap;
-use namaco::parser;
 use std::fs;
 
 fn main() {
@@ -9,16 +8,41 @@ fn main() {
         .version("0.0.1")
         .author("Nakano Masaki<namachan10777@gmail.com>")
         .about("morphological analyzer")
-        .arg(clap::Arg::with_name("DICT")
-            .help("set naist-jdic.csv")
-            .required(true)
+        .subcommand(clap::SubCommand::with_name("compile")
+                    .about("compile assets")
+                    .arg(clap::Arg::with_name("DICT")
+                         .short("d")
+                         .long("dict")
+                         .value_name("DICT")
+                         .required(true)
+                         .help("pass csv file for mecab"))
+                    .arg(clap::Arg::with_name("MATRIX")
+                         .short("m")
+                         .long("matrix")
+                         .value_name("MATRIX")
+                         .required(true)
+                         .help("pass matrix file for mecab"))
+                    .arg(clap::Arg::with_name("OUTPUT")
+                         .short("o")
+                         .long("output")
+                         .value_name("OUTPUT")
+                         .required(true)
+                         .help("specify output file name")))
+        .arg(clap::Arg::with_name("ASSET")
+            .help("pass asset file")
             .index(1))
         .get_matches();
-    let file = fs::File::open(matches.value_of("DICT").unwrap()).unwrap();
-    let cfg = parser::DictCfg {
-        word: 0,
-        matrix_id: 2,
-        gencost: 3,
-    };
-    let _trie = parser::build_trie(&file, &cfg, |arr| arr[3].trim().to_string()).unwrap();
+    if let Some(matches) = matches.subcommand_matches("compile") {
+        let mut dict_file = fs::File::open(matches.value_of("DICT").unwrap()).unwrap();
+        let mut matrix_file = fs::File::open(matches.value_of("MATRIX").unwrap()).unwrap();
+        let mut output_file = fs::File::create(matches.value_of("OUTPUT").unwrap()).unwrap();
+        let cfg = namaco::parser::DictCfg {
+            gencost: 3,
+            matrix_id: 1,
+            word: 0,
+        };
+        let meta = fs::metadata(matches.value_of("DICT").unwrap()).unwrap();
+        let morph = namaco::Morph::from_text(&mut matrix_file, &mut dict_file, &cfg, |arr| arr.join(","), Some(meta.len() as usize)).unwrap();
+        morph.export(&mut output_file).unwrap();
+    }
 }
