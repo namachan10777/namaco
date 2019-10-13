c6039de3d5f66ff9c0ac6531b15849c37bde3627
namachan10777 <namachan10777@gmail.com>
2019-10-13 22:46:26 +0900

    disable dynamic update

--- src/trie.rs
+++ src/trie.rs
@@ -30,14 +30,6 @@ impl Node {
         Node::from(DecodedNode::Sec(check, base, id))
     }
 
-    fn node(check: usize, base: usize, id: usize) -> Self {
-        Node {
-            check,
-            base,
-            id,
-        }
-    }
-
     fn blank() -> Self {
         Node::default()
     }
@@ -174,14 +166,13 @@ pub struct Trie<T: Serialize> {
 }
 
 const ROW_LEN: usize = 256;
-type Row = [Node; ROW_LEN];
 
 impl<T: Serialize> Trie<T> {
     pub fn new() -> Trie<T> {
-        let mut tree = vec![Node::blank(); 256];
+        let mut tree = vec![Node::blank(); ROW_LEN];
         tree[0] = Node::root(0);
         Trie {
-            cache: vec![0;256],
+            cache: vec![0;ROW_LEN],
             capacities: vec![254],
             tree,
             storage: Vec::new(),
@@ -229,7 +220,7 @@ mod test_explore {
         tree[7] = Node::sec(2, 4, Some(3));
         tree[5] = Node::term(7, 4);
         let trie = Trie {
-            cache: vec![0;256],
+            cache: vec![0;ROW_LEN],
             capacities: vec![249, 255],
             tree,
             storage: Vec::new() as Vec<Vec<String>>,
@@ -248,13 +239,13 @@ mod test_explore {
 impl<T: Serialize> Trie<T> {
     // To reallocate base and expand tree if need to do.
     // FIXME bottleneck
-    fn reallocate_base(&mut self, target: &[bool; 256], cnt: u8) -> usize {
+    fn reallocate_base(&mut self, target: &[bool; ROW_LEN], cnt: u8) -> usize {
         for block_idx in self.cache[cnt as usize]..self.capacities.len() {
             if self.capacities[block_idx] >= cnt {
-                for innser_offset in 0..256 {
+                for innser_offset in 0..ROW_LEN {
                     let mut safe = true;
                     let offset = (block_idx << 8) | innser_offset;
-                    for target_idx in 0..256 {   
+                    for target_idx in 0..ROW_LEN {   
                         if target[target_idx] && DecodedNode::Blank != Into::<DecodedNode>::into(self.tree[offset ^ target_idx].clone()) {
                             safe = false; 
                             break;                   
@@ -283,12 +274,12 @@ mod test_reallocate_base {
     use super::*;
     #[test]
     fn test_reallocate_base() {
-        let mut mask = [false; 256];
+        let mut mask = [false; ROW_LEN];
         mask[0] = true;
         let mut tree = vec![Node::term(0, 0); 512];
         tree[6] = Node::blank();
         let mut trie: Trie<String> = Trie {
-            cache: vec![0;256],
+            cache: vec![0;ROW_LEN],
             capacities: vec![1, 0],
             tree,
             storage: Vec::new(),
@@ -324,284 +315,6 @@ mod test_reallocate_base {
 }
 
 impl<T: Serialize> Trie<T> {
-    fn read_row(&self, parent_idx: usize) -> Row {
-        let mut buf: Row = [Node::blank(); 256];
-        let base = self.tree[parent_idx].base;
-        for i in 0..256 {
-            if self.tree[base ^ i].check == parent_idx {
-                buf[i] = self.tree[base ^ i];
-            }
-        }
-        buf
-    }
-
-    fn erase_row(&mut self, parent_idx: usize) {
-        let base = self.tree[parent_idx].base;
-        for i in 0..256 {
-            if self.tree[base ^ i].check == parent_idx {
-                self.tree[base ^ i] = Node::blank();
-            }
-        }
-    }
-
-    fn count_children(&self, parent_idx: usize) -> usize {
-        let mut cnt = 0usize;
-        let base = self.tree[parent_idx].base;
-        for i in 0..256 {
-            if self.tree[base ^ i].check == parent_idx {
-                cnt += 1;
-            }
-        }
-        cnt
-    }
-}
-
-#[cfg(test)]
-mod test_read_erase_row {
-    use super::*;
-    #[test]
-    fn test_read () {
-        let mut tree = [Node::blank(); 512].to_vec();
-        tree[0] = Node::root(0);
-        tree[1] = Node::sec(0, 64, None);
-        tree[2] = Node::term(0, 0);
-        tree[64] = Node::term(1, 0);
-        let trie: Trie<String> = Trie {
-            cache: vec![0;256],
-            capacities: vec![251, 255],
-            tree,
-            storage: Vec::new(),
-        };
-
-        let row1 = trie.read_row(0).to_vec();
-        let mut row1_ans = vec![Node::blank();256];
-        row1_ans[1] = Node::sec(0, 64, None);
-        row1_ans[2] = Node::term(0, 0);
-        assert_eq!(row1, row1_ans);
-
-        let row2 = trie.read_row(1).to_vec();
-        let mut row2_ans = vec![Node::blank();256];
-        row2_ans[0] = Node::term(1, 0);
-        assert_eq!(row2, row2_ans)
-    }
-
-    #[test]
-    fn test_erase () {
-        let mut tree = [Node::blank(); 512].to_vec();
-        tree[0] = Node::root(0);
-        tree[1] = Node::sec(0, 64, None);
-        tree[2] = Node::term(0, 0);
-        tree[64] = Node::term(1, 0);
-        let mut trie: Trie<String> = Trie {
-            cache: vec![0;256],
-            capacities: vec![251, 255],
-            tree,
-            storage: Vec::new(),
-        };
-
-        trie.erase_row(0);
-        let mut tree1 = [Node::blank(); 512].to_vec();
-        tree1[0] = Node::root(0);
-        tree1[64] = Node::term(1, 0);
-        assert_eq!(trie.tree, tree1);
-    }
-}
-
-impl<T: Serialize> Trie<T> {
-    // This function forcely overwrite tree
-    // 存在しなかったのにrowに入っているとfromを誤認する
-    fn paste(&mut self, row: Row, addition: Row, from: usize) -> usize {
-        // make mask
-        let mut mask = [false; 256];
-        let mut cnt = 0;
-        for i in 0..256 {
-            if row[i].check != NO_PARENT
-                || row[i].base != NO_CHILD
-                || addition[i].check != NO_PARENT
-                || addition[i].base != NO_CHILD {
-                mask[i] = true;
-                cnt += 1;
-            }
-        }
-        let to = self.reallocate_base(&mask, cnt);
-        self.capacities[to >> 8] -= cnt;
-        // place
-        for i in 0..256 {
-            if row[i].check != NO_PARENT {
-                // place bro
-                self.tree[to ^ i] = row[i];
-                // update children's check
-                for j in 0..256 {
-                    if row[i].base != NO_CHILD && self.tree[row[i].base ^ j].check == from ^ i {
-                        self.tree[row[i].base ^ j].check = to ^ i;
-                    }
-                }
-            }
-        }
-        // additional placement without updation of children's check
-        for i in 0..256 {
-            if addition[i].check != NO_PARENT || addition[i].base != NO_CHILD {
-                self.tree[to ^ i] = addition[i];
-            }
-        }
-        to
-    }
-}
-
-#[cfg(test)]
-mod test_paste {
-    // TODO improve test case
-    use super::*;
-    #[test]
-    fn test_paste() {
-        let mut tree = vec![Node::blank(); 512];
-        tree[0] = Node::root(0);
-        tree[64] = Node::term(5, 0);
-        
-        let mut row = [Node::blank(); 256];
-        row[1] = Node::sec(0, 64, None);
-        row[2] = Node::term(1, 0);
-
-        let mut trie: Trie<String> = Trie {
-            cache: vec![0;256],
-            capacities: vec![253, 255],
-            tree,
-            storage: Vec::new(),
-        };
-
-        assert_eq!(trie.paste(row, [Node::default();256], 4), 0);
-        let mut ans = vec![Node::blank(); 512];
-        ans[0] = Node::root(0);
-        ans[64] = Node::term(1, 0);
-        ans[1] = Node::sec(0, 64, None);
-        ans[2] = Node::term(1, 0);
-
-        assert_eq!(trie.tree, ans);
-
-        let mut tree2 = vec![Node::blank(); 512];
-        tree2[0] = Node::root(0);
-        let mut trie2: Trie<String> = Trie {
-            cache: vec![0;256],
-            capacities: vec![251, 255],
-            tree: tree2,
-            storage: Vec::new(),
-        };
-        let mut row = [Node::blank(); 256];
-        row[0] = Node::sec(0, 0, None);
-        assert_eq!(trie2.paste([Node::blank(); 256], row, 0), 1);
-        let mut ans = vec![Node::blank(); 512];
-        ans[0] = Node::root(0);
-        ans[1] = Node::sec(0, 0, None);
-        assert_eq!(&trie2.tree, &ans);
-    }
-}
-
-impl<T: Serialize> Trie<T> {
-    fn insert_by_push_out(&mut self, target_idx: usize, parent_idx: usize) -> usize {
-        let parent = self.tree[parent_idx];
-        let target = self.tree[target_idx];
-
-        if target.check == NO_PARENT {
-            if target.base == NO_CHILD {
-                return target_idx;
-            }
-            else {
-                unreachable!();
-            }
-        }
-
-        let old_base = if parent.check < self.tree.len() {
-            self.tree[parent.check].base
-        }
-        else {
-            NO_CHILD
-        };
-        let row = self.read_row(target.check);
-        self.erase_row(target.check);
-        let parent_moved = self.tree[parent_idx] == Node::blank();
-        // insert dummy
-        self.tree[target_idx] = Node::node(0, NO_CHILD, NO_ITEM);
-        // replace parent
-        let new_base = self.paste(row, [Node::blank(); 256], self.tree[target.check].base);
-        // update parent of target
-        self.tree[target.check].base = new_base;
-        // if parent was included in target of push_out
-        // 親のcheckが変わっただけでもここの判定に入ってしまう
-        if parent_moved {
-            // old_base ^ parent_idx: relative position
-            // (old_base ^ parent_idx) ^ new_base: new absolute position
-            // A ^ B = C ⇒ C ^ A = B ∩ C ^ B = A
-            self.tree[target_idx] = Node::term(old_base ^ parent_idx ^ new_base, NO_ITEM);
-        }
-        else {
-            self.tree[target_idx] = Node::term(parent_idx, NO_ITEM);
-        }
-        target_idx
-    }
-
-    fn insert_by_slide_brothers(&mut self, target_idx: usize, parent_idx: usize) -> usize {
-        let parent = self.tree[parent_idx];
-        let row = self.read_row(parent_idx);
-        self.erase_row(parent_idx);
-        let mut addition = [Node::blank(); 256];
-        addition[parent.base ^ target_idx] = Node::node(parent_idx, NO_CHILD, NO_ITEM);
-        let new_base = self.paste(row, addition, parent.base);
-        self.tree[parent_idx].base = new_base;
-        target_idx ^ parent.base ^ new_base
-    }
-}
-
-impl<T: Serialize> Trie<T> {
-    pub fn add(&mut self, way: &[u8], cargo: T) -> Result<(), ()> {
-        let mut parent_idx = 0;
-        for octet in way {
-            if self.tree[parent_idx].base == NO_CHILD {
-                self.tree[parent_idx].base = 0;
-            }
-            let child_idx = self.tree[parent_idx].base ^ (*octet as usize);
-            let child = self.tree[child_idx];
-            if child.check == NO_PARENT {
-                if child.base == NO_CHILD {
-                    self.tree[child_idx] = Node::node(parent_idx, NO_CHILD, NO_ITEM);
-                    parent_idx = child_idx;
-                }
-                // root case
-                else {
-                    parent_idx = self.insert_by_slide_brothers(child_idx, parent_idx);
-                }
-            }
-            else {
-                if child.check == parent_idx {
-                    parent_idx = child_idx;
-                }
-                // conflict case
-                else {
-                    let brother_num  = self.count_children(parent_idx);
-                    let stranger_num = self.count_children(child.check);
-                    parent_idx =
-                        if brother_num > stranger_num {
-                            self.insert_by_push_out(child_idx, parent_idx)
-                        }
-                        else {
-                            self.insert_by_slide_brothers(child_idx, parent_idx)
-                        };
-                }
-            }
-        }
-        self.tree[parent_idx].id =
-            if self.tree[parent_idx].id == NO_ITEM {
-                self.storage.push(vec![cargo]);
-                self.storage.len() - 1
-            }
-            else {
-                self.storage[self.tree[parent_idx].id].push(cargo);
-                self.tree[parent_idx].id
-            };
-        Ok(())
-    }
-}
-
-impl<T: Serialize> Trie<T> {
     pub fn find(&self, way: &[u8]) -> Result<&[T], ()> {
         match self.explore(way) {
             Ok(idx) => {
@@ -615,74 +328,6 @@ impl<T: Serialize> Trie<T> {
             },
             Err(_) => Err(())
         }
-    }
-}
-
-#[cfg(test)]
-mod test_add_find {
-    use super::*;
-    #[test]
-    fn test_add_find() {
-        let mut trie: Trie<String> = Trie::new();
-        trie.add(&[2, 1], String::from("21")).unwrap();
-        trie.add(&[1, 1], String::from("11")).unwrap();
-        trie.add(&[1, 2, 3], String::from("123")).unwrap();
-        trie.add(&[0], String::from("0")).unwrap();
-        trie.add(&[0, 0], String::from("00")).unwrap();
-        trie.add(&[1, 2], String::from("12")).unwrap();
-        trie.add(&[1, 2, 0], String::from("120")).unwrap();
-        trie.add(&[3, 1, 3], String::from("313")).unwrap();
-        trie.add(&[1, 6, 1], String::from("161")).unwrap();
-        trie.add(&[0, 1], String::from("01")).unwrap();
-        trie.add(&[2, 0], String::from("20")).unwrap();
-
-        assert_eq!(trie.find(&[0]), Ok(&[String::from("0")][..]));
-        assert_eq!(trie.find(&[0]), Ok(&[String::from("0")][..]));
-        assert_eq!(trie.find(&[0, 0]), Ok(&[String::from("00")][..]));
-        assert_eq!(trie.find(&[1, 2, 3]), Ok(&[String::from("123")][..]));
-        assert_eq!(trie.find(&[1, 2]), Ok(&[String::from("12")][..]));
-        assert_eq!(trie.find(&[1, 2, 0]), Ok(&[String::from("120")][..]));
-        assert_eq!(trie.find(&[3, 1, 3]), Ok(&[String::from("313")][..]));
-        assert_eq!(trie.find(&[1, 6, 1]), Ok(&[String::from("161")][..]));
-        assert_eq!(trie.find(&[0, 1]), Ok(&[String::from("01")][..]));
-        assert_eq!(trie.find(&[2, 0]), Ok(&[String::from("20")][..]));
-        assert_eq!(trie.find(&[2, 1]), Ok(&[String::from("21")][..]));
-        assert_eq!(trie.find(&[1]), Err(()));
-        assert_eq!(trie.find(&[7, 4]), Err(()));
-    }
-
-    #[test]
-    fn test_add() {
-        let mut trie: Trie<String> = Trie::new();
-        trie.add("張り込め".as_bytes(), String::from("張り込め")).unwrap();
-        trie.add("ニッカーボッカー".as_bytes(), String::from("ニッカーボッカー")).unwrap();
-        trie.add("証城寺".as_bytes(), String::from("証城寺")).unwrap();
-        trie.add("差し昇っ".as_bytes(), String::from("差し登っ")).unwrap();
-        trie.add("抜け出せれ".as_bytes(), String::from("抜け出せれ")).unwrap();
-        trie.add("たい".as_bytes(), String::from("たい")).unwrap();
-        trie.add("アオガエル".as_bytes(), String::from("アオガエル")).unwrap();
-        trie.add("長府浜浦".as_bytes(), String::from("長府浜浦")).unwrap();
-        trie.add("中佃".as_bytes(), String::from("中佃")).unwrap();
-        trie.add("幻視".as_bytes(), String::from("幻視")).unwrap();
-        trie.add("小船木".as_bytes(), String::from("小船木")).unwrap();
-        trie.add("浅黒かれ".as_bytes(), String::from("浅黒かれ")).unwrap();
-        trie.add("扁かろ".as_bytes(), String::from("扁かろ")).unwrap();
-        trie.add("咲き乱れ".as_bytes(), String::from("咲き乱れ")).unwrap();
-
-        assert_eq!(trie.find("張り込め".as_bytes()), Ok(&[String::from("張り込め")][..]));
-        assert_eq!(trie.find("ニッカーボッカー".as_bytes()), Ok(&[String::from("ニッカーボッカー")][..]));
-        assert_eq!(trie.find("証城寺".as_bytes()), Ok(&[String::from("証城寺")][..]));
-        assert_eq!(trie.find("差し昇っ".as_bytes()), Ok(&[String::from("差し登っ")][..]));
-        assert_eq!(trie.find("抜け出せれ".as_bytes()), Ok(&[String::from("抜け出せれ")][..]));
-        assert_eq!(trie.find("たい".as_bytes()), Ok(&[String::from("たい")][..]));
-        assert_eq!(trie.find("アオガエル".as_bytes()), Ok(&[String::from("アオガエル")][..]));
-        assert_eq!(trie.find("長府浜浦".as_bytes()), Ok(&[String::from("長府浜浦")][..]));
-        assert_eq!(trie.find("中佃".as_bytes()), Ok(&[String::from("中佃")][..]));
-        assert_eq!(trie.find("幻視".as_bytes()), Ok(&[String::from("幻視")][..]));
-        assert_eq!(trie.find("小船木".as_bytes()), Ok(&[String::from("小船木")][..]));
-        assert_eq!(trie.find("浅黒かれ".as_bytes()), Ok(&[String::from("浅黒かれ")][..]));
-        assert_eq!(trie.find("扁かろ".as_bytes()), Ok(&[String::from("扁かろ")][..]));
-        assert_eq!(trie.find("咲き乱れ".as_bytes()), Ok(&[String::from("咲き乱れ")][..]));
     }
 }
 
@@ -714,9 +359,9 @@ impl<T: Serialize + Clone> Trie<T> {
     }
 
     pub fn add_static(&mut self, src: &[(&[u8], T)], select: usize, parent_idx: usize) -> usize {
-        let mut row = [Node::default(); 256];
-        let mut mask = [false; 256];
-        let mut update = [false;256];
+        let mut row = [Node::default(); ROW_LEN];
+        let mut mask = [false; ROW_LEN];
+        let mut update = [false;ROW_LEN];
         let mut before = -1i16;
         let mut cnt = 0u8;
 
@@ -742,12 +387,12 @@ impl<T: Serialize + Clone> Trie<T> {
         }
 
         let base = self.reallocate_base(&mask, cnt);
-        for i in 0..256 {
+        for i in 0..ROW_LEN {
             if mask[i] {
                 self.tree[i ^ base] = row[i];
             }
         }
-        for i in 0..256 {
+        for i in 0..ROW_LEN {
             if update[i] {
                 let idx = i ^ base;
                 self.tree[idx].base = self.add_static(Self::get_domain(src, select, i as u8), select+1, idx);
@@ -827,7 +472,7 @@ mod test_static_construction {
     }
 
     #[test]
-    fn test_add_static() {
+    fn test_add_find() {
         let trie = Trie::static_construction(&mut vec![
             ("咲き乱れ".as_bytes(), String::from("咲き乱れ")),
             ("張り込め".as_bytes(), String::from("張り込め")),
