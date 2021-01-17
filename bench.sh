1dd54a61610faef563a28f0462255fe3f333b501
Nakano Masaki <namachan10777@gmail.com>
2021-01-17 12:24:48 +0900

    fix bench.sh

--- bench.sh
+++ bench.sh
@@ -5,4 +5,4 @@ cargo build --release
 
 cat ./naist-jdic/naist-jdic.csv | shuf -n 400000 | nkf > ./short.csv
 
-time ./target/release/namaco ./short.csv
+time ./target/release/namaco compile --dict ./short.csv --matrix ./naist-jdic/matrix.def --output dict
